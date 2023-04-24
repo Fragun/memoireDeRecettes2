0841ee058f179d2291e917d9013ac944a21df7f8
David <dethoordavid@yahoo.fr>
2023-04-24 18:52:58 +0200

    register ok login ko

--- back_memoireDeRecettes/routes/api/auth.js
+++ back_memoireDeRecettes/routes/api/auth.js
@@ -7,27 +7,32 @@ const connection = require("../../database/index");
 
 
 router.post("/", async (req, res) => {
-  const { email, password } = req.body;
-
+  const email = req.body;
+  const passwordEnter = req.body.password;
   const sqlVerify = `SELECT * FROM user WHERE USER_EMAIL="${email}"`;
 console.log(sqlVerify);
 
   connection.query(sqlVerify, (err, result) => {
     console.log(result[0].USER_PASSWORD);
-    
+    let userPasswordDatabase = result[0].USER_PASSWORD;
 
     if (err) throw err;
     if (result.length === 0) {
       console.log("utilisateur non existant");
-      result[0] = {};
-      result[0].condition = false;
-      res.send(JSON.stringify(result[0]));
-    } else {
-      console.log("utilisateur existant");
-      const message = "utilisateur existant";
-      result[0].message = message;
-      result[0].condition = true;
-      res.send(JSON.stringify(result[0]));
+      // result[0] = {};
+      // result[0].condition = false;
+      // res.send(JSON.stringify(result[0]));
+      res.status(400).json("Email et/ou mot de passe incorrect");
+    } else if (bcrypt.compareSync(passwordEnter, userPasswordDatabase)) {
+      const token = jsonwebtoken.sign({}, key, {
+        subject: user._id.toString(),
+        expiresIn: 3600 * 24 * 30 * 6,
+        algorithm: "RS256",
+        //sameSite: 'none',
+        //sameSite: 'Lax',
+      });
+      res.cookie("token", token);
+      //res.json(user);
     }
   });
 });
